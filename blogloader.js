f6ab7096996704dedf01c6c7bc458eecd598423d
proxdxd03 <98048238+proxdxd03@users.noreply.github.com>
2023-02-25 00:17:51 +0100

    0.0.25: Something's broken

--- blogloader.js
+++ blogloader.js
@@ -1,8 +1,27 @@
-var home_blogentries = ["24022023.html"];
-//hmmmmmmmmm
-export function addBlogEntries(document, elementId) {
-    for(var i = 0; i < home_blogentries.length; i++) {
-        console.log(home_blogentries[i]);
-        $(`#${id}`).load(home_blogentries[i]);
+var misc_blogentries = ["24_02_2023_0.html"];
+var concert_blogentries = [];
+var release_blogentries = [];
+export function addBlogEntries(elementId, category) {
+    switch(category) {
+        case "all": 
+            addBlogEntriesByCategory(elementId, misc_blogentries)
+            addBlogEntriesByCategory(elementId, concert_blogentries)
+            addBlogEntriesByCategory(elementId, release_blogentries)
+            break;
+        case "misc": 
+            addBlogEntriesByCategory(elementId, misc_blogentries)
+            break;
+        case "concert": 
+            addBlogEntriesByCategory(elementId, concert_blogentries)
+            break;
+        case "release": 
+            addBlogEntriesByCategory(elementId, release_blogentries)
+            break;
+    }
+}
+
+function addBlogEntriesByCategory(elementId, src) {
+    for(var i = 0; i < src.length; i++) {
+        $(`#${elementId}`).load(src[i]);
     }
 }
