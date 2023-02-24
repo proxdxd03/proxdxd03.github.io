cff01f7871c93a14bcb2af384e0998bc1c671732
proxdxd03 <98048238+proxdxd03@users.noreply.github.com>
2023-02-24 23:35:06 +0100

    0.0.22: Testing again

--- blogloader.js
+++ blogloader.js
@@ -1,6 +1,6 @@
 var home_blogentries = ["24022023.html"];
 export function addBlogEntries(document, elementId) {
-    for(i = 0; i < home_blogentries.length; i++) {
+    for(var i = 0; i < home_blogentries.length; i++) {
         console.log(home_blogentries[i]);
         $(`#${id}`).load(home_blogentries[i]);
     }
