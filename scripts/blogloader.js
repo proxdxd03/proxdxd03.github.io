1731cb357630496b20cdc783862039aad4c7c8b6
proxdxd03 <98048238+proxdxd03@users.noreply.github.com>
2023-02-25 11:13:47 +0100

    0.0.30: Testing basic ajax

--- scripts/blogloader.js
+++ scripts/blogloader.js
@@ -25,3 +25,32 @@ function addBlogEntriesByCategory(elementId, src) {
         $(`#${elementId}`).load(`posts/${src[i]}`); 
     }
 }
+
+export function loadFromJSON(elementId, category) {
+    $.ajax({
+        type:    "GET",
+        url:     "posts/posts.json",
+        success: function(text) {
+            json = JSON.parse(text);
+            array = json.misc;
+            document.getElementById(elementId).innerHTML = array[0];
+        },
+        error:   function() {
+            console.log("BRUH")
+        }
+    });
+}
+
+function create(htmlStr) {
+    var frag = document.createDocumentFragment(),
+        temp = document.createElement('div');
+    temp.innerHTML = htmlStr;
+    while (temp.firstChild) {
+        frag.appendChild(temp.firstChild);
+    }
+    return frag;
+}
+
+var fragment = create('<div>Hello!</div><p>...</p>');
+// You can use native DOM methods to insert the fragment:
+document.body.insertBefore(fragment, document.body.childNodes[0]);
